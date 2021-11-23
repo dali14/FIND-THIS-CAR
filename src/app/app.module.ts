593ff67157a492fae2165bcaf6df734d4ef0978a
DELL <dalijlasii@gmail.com>
2021-11-23 18:32:00 +0100

    AppModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,6 @@ import { AppRoutingModule } from './approuting.module';
     NavbarComponent,
     HomeComponent,
     LoginComponent
-    
-    
   ],
   imports: [
     BrowserModule,
--- src/app/approuting.module.ts
+++ src/app/approuting.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -16,9 +15,9 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
   declarations: [
-    NotFoundComponent,
+    /**NotFoundComponent,
     LoginComponent,
-    NavbarComponent
+    NavbarComponent*/
   ]
 })
 export class AppRoutingModule {

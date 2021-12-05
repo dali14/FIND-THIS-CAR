b4e988a66140050117ed558b08be867926c5faa0
DELL <dalijlasii@gmail.com>
2021-12-05 21:46:45 +0100

    addlistadmintoadminmodule

---
+++ src/app/admin.list.ts
@@ -0,0 +1,50 @@
+import { Admin } from "./admin.module";
+
+export const admin:Admin[] =[
+{id: 1,
+ name: "dali",
+ lastname: "ariana",
+ email: "dali@dali.com",
+ password: "123456",
+ tel:"20154458",
+ adresse:"2rue wajda",
+ ville:"manouba",
+ codepostal:"2010",
+ role:"admin"
+},
+{id: 2,
+    name: "dali",
+    lastname: "ariana",
+    email: "dali@dali.com",
+    password: "123456",
+    tel:"20154458",
+    adresse:"2rue wajda",
+    ville:"manouba",
+    codepostal:"2010",
+    role:"admin"
+   },
+
+   {id: 3,
+    name: "dali",
+    lastname: "ariana",
+    email: "dali@dali.com",
+    password: "123456",
+    tel:"20154458",
+    adresse:"2rue wajda",
+    ville:"manouba",
+    codepostal:"2010",
+    role:"admin"
+   },
+
+   {id: 4,
+    name: "dali",
+    lastname: "ariana",
+    email: "dali@dali.com",
+    password: "123456",
+    tel:"20154458",
+    adresse:"2rue wajda",
+    ville:"manouba",
+    codepostal:"2010",
+    role:"admin"
+   }
+]
---
+++ src/app/admin.module.ts
@@ -0,0 +1,13 @@
+export interface Admin {
+    id: number;
+    name: string;
+    lastname: string;
+    email: string;
+    password: string;
+    tel:string;
+    adresse:string;
+    ville: string;
+    codepostal:string;
+    role: string;
+    
+  }

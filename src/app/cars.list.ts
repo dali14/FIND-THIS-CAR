133f17962d33160a02b69785265d98ec88a2b881
DELL <dalijlasii@gmail.com>
2021-12-05 23:11:09 +0100

    addcarsmodellist

---
+++ src/app/cars.list.ts
@@ -0,0 +1,12 @@
+import { Cars} from "./cars.module" ;
+
+export const cars:Cars[] =[
+{
+    mat1:"150",
+    mat2:"5874",
+    model:"audi",
+    pseudouser:"dali1",
+}
+
+
+]
---
+++ src/app/cars.module.ts
@@ -0,0 +1,6 @@
+export interface Cars {
+    mat1:string;
+    mat2:string;
+    model:string;
+    pseudouser:string;
+}
